6f8f71fbddf32c7ee8af161f7bb70058d2743f88
ruanwen123456 <1300945176@qq.com>
2019-07-13 17:39:29 +0800

    springboot2.0搭建改成https

---
+++ src/main/java/com/ruanhao/springbootvue/Hello.java
@@ -0,0 +1,12 @@
+package com.ruanhao.springbootvue;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+	@GetMapping("/hello")
+	public String sayHello() {
+		return "hello";
+	}
+}
---
+++ src/main/java/com/ruanhao/springbootvue/SpringbootvueApplication.java
@@ -0,0 +1,13 @@
+package com.ruanhao.springbootvue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootvueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootvueApplication.class, args);
+	}
+
+}
